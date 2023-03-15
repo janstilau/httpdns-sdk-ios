a3943e43eb0105bd801775ccf462d77b575464f2
liuguoqiang <janstilau@gmail.com>
2023-03-15 20:40:51 +0800

    sync

--- DNSDemo/DNSDemo/ViewController.mm
+++ DNSDemo/DNSDemo/ViewController.mm
@@ -7,6 +7,41 @@
 
 #import "ViewController.h"
 #import <MSDKDns_C11/MSDKDns.h>
+/*
+ DES加密    支持中
+ 密钥
+ EAUr0zW5
+ 
+ AES加密    支持中
+ 密钥
+ ULEK7rPGDaP788Eh
+ 
+ HTTPS加密    支持中
+ Token
+ 711886787
+ 
+ 标签
+ 应用名称    备注             iOS APPID               安卓 APPID        创建时间
+ Yami    Yami 点餐系统    3246PPDWT0X92DYE    44T7XRWKTLWR0MFI    2023-03-15 14:30:42
+ */
+
+/*
+ DES加密    支持中
+ 密钥
+ EAUr0zW5
+ 
+ AES加密    支持中
+ 密钥
+ ULEK7rPGDaP788Eh
+ 
+ HTTPS加密    支持中
+ Token
+ 711886787
+ 
+ 
+ 应用名称    备注         iOS APPID           安卓 APPID            创建时间
+ Wisdom    智慧教育    TPUY9Z8F73PNOLK8    F02OCLZCTI094YS3    2023-03-15 20:33:00
+ */
 
 @interface ViewController ()
 
