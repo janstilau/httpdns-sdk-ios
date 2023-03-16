9bb8b0c3ebd0c2b8c9cbedd8f6a7cead50645c87
liuguoqiang <janstilau@gmail.com>
2023-03-16 10:40:27 +0800

    mod: sync

---
+++ DNSDemo/wisdom_result.h
@@ -0,0 +1,26 @@
+//
+//  wisdom_result.h
+//  DNSDemo
+//
+//  Created by liuguoqiang on 2023/3/15.
+//
+
+#ifndef wisdom_result_h
+#define wisdom_result_h
+
+/*
+ hardware-app.youdao.com
+ https://119.29.29.99/d?dn=hardware-app.youdao.com&token=711886787&ttl=1
+ -> 103.74.50.105;103.74.50.106;103.74.50.134;103.74.50.133,600
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ */
+#endif /* wisdom_result_h */
